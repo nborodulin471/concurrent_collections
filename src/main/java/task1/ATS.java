75ba3e6576072714f6709c2bf1af98b8d0f158db
nick <nborodulin@gmail.com>
2022-03-08 13:01:56 +0700

    Исправление CR

--- src/main/java/task1/ATS.java
+++ src/main/java/task1/ATS.java
@@ -1,19 +1,25 @@
 package task1;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import static task1.Main.*;
 
 public class ATS implements Runnable {
+    private ConcurrentLinkedQueue<String> queue;
+
+    public ATS(ConcurrentLinkedQueue<String> queue) {
+        this.queue = queue;
+    }
 
     @Override
     public synchronized void run() {
         try {
             for (int i = 0; i < COUNT_CALLS; i++) {
                 String msg = "тест" + i;
-                CONCURRENT_LINKED_QUEUE.add("тест" + i);
+                queue.add("тест" + i);
                 System.out.println("Поступил звонок с сообщением " + msg);
                 Thread.sleep(TIME_WAIT_CALL);
             }
-            notify();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
